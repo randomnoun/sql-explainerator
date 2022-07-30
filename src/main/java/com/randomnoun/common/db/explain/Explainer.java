4a9b3e0c7280235931cbb311e8e31deb4539175a
Greg Knox <knoxg@randomnoun.com>
2022-07-31 08:21:57 +1000

    implement writeHtml for most of the nodes (wip); external css

--- src/main/java/com/randomnoun/common/db/explain/Explainer.java
+++ src/main/java/com/randomnoun/common/db/explain/Explainer.java
@@ -6,9 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
@@ -32,6 +35,21 @@ public class Explainer {
 
 	static Logger logger = Logger.getLogger(Explainer.class);
 	
+	private static enum AccessTypeEnum {
+		FULL_TABLE_SCAN("ALL"),
+		NON_UNIQUE_KEY("ref"),
+		UNIQUE_KEY("eq_ref");
+		private String jsonValue;
+		private AccessTypeEnum(String jsonValue) {
+			this.jsonValue = jsonValue;
+		}
+		public static AccessTypeEnum fromJsonValue(String jsonValue) {
+			for (AccessTypeEnum e : AccessTypeEnum.values()) {
+				if (e.jsonValue.equals(jsonValue)) { return e; }
+			}
+			throw new IllegalArgumentException("no AccessTypeEnum with jsonValue '" + jsonValue + "'");
+		}
+	}
 
 	private static class NameList extends ArrayList<String> implements Struct.ToJson {
 
@@ -170,9 +188,21 @@ public class Explainer {
 			return s;
 		}
 		protected abstract void writeHtml(PrintWriter pw);
+		
+		protected Stream<Node> reverseStream(List<Node> children) {
+			// why the hell isn't this on https://stackoverflow.com/questions/24010109/java-8-stream-reverse-order 
+			// ?
+			return IntStream.range(0, children.size())
+				.mapToObj(i -> children.get(children.size() - i - 1)); 
+		}
+
 	}
 	
 	public static class QueryBlockNode extends Node {
+		
+		public Object selectId;
+		public Object message;
+		
 		public QueryBlockNode() {
 			super("query_block", false);
 		}
@@ -182,19 +212,23 @@ public class Explainer {
 			pw.println("<table display=\"display: inline-table;\">"); // of course it is
 			CostInfo ci = (CostInfo) this.attributes.get("costInfo");
 			if (ci != null) {
-				pw.println("<tr><td>Query cost: " + ci.getQueryCost() + "</td></tr>"); 
+				pw.println("<tr><td><div class=\"queryCost\">Query cost: " + ci.getQueryCost() + "</div></td></tr>"); 
 			}
-			pw.println("<tr><td>[ query_block ]</td></tr>"); // tooltip = select ID & query cost
-			pw.println("<tr><td>   ^</td></tr>");
-			
+			String label = "query_block" + (selectId == null ? "" : " #" + selectId);
+			String clazz = (selectId == null ? " topNode" : "");
+			pw.println("<tr><td><div class=\"queryBlock " + clazz + "\">" + label + "</div></td></tr>"); // tooltip = select ID & query cost
+			if (message != null) {
+				pw.println("<tr><td>" + message + "</td></tr>");
+			}
 			// the thing inside the query block
 			if (this.children.size() > 0) {
+				pw.println("<tr><td><div class=\"upArrow\"></div></td></tr>");
 				pw.println("<tr>");
-				for (Node c : this.children) {
+				reverseStream(children).forEach( c -> {
 					pw.println("<td>");
 					c.writeHtml(pw);
 					pw.println("</td>");
-				}
+				});
 				pw.println("</tr>");
 			}
 			pw.println("</td></tr>");
@@ -203,25 +237,29 @@ public class Explainer {
 	}
 	
 	public static class UnionResultNode extends Node {
+		public String tableName;
 		public UnionResultNode() {
 			super("union_result", false); // TODO subtypes
 		}
 		protected void writeHtml(PrintWriter pw) {
-			pw.println("<table display=\"display: inline-table;\">"); // of course it is
-			pw.println("<tr><td style=\"background-color: #99999;\">UNION</td></tr>"); // tooltip: tableName, usingTemporaryTable: true
-			pw.println("<tr><td>" + attributes.get("tableName") + "</td></tr>");
 			QuerySpecificationsNode qsn = (QuerySpecificationsNode) this.children.get(0);
+			
+			pw.println("<table display=\"display: inline-table;\">"); // of course it is
+			String colspan = " colspan=\"" + Math.max(1, qsn.children.size()) + "\"";
+			pw.println("<tr><td " + colspan + "><div class=\"union\">UNION</div></td></tr>"); // tooltip: tableName, usingTemporaryTable: true
+			pw.println("<tr><td " + colspan + "><div class=\"tableName\">" + Text.escapeHtml(tableName) + "</div></td></tr>");
 			if (qsn.children.size() > 0) {
 				pw.println("<tr>");
-				for (Node c : qsn.children) {
+				// for (Node c : qsn.children) {
+				reverseStream(qsn.children).forEach( c -> {
 					pw.println("<td>");
-					pw.println("<div>^</div>");
+					pw.println("<div class=\"upArrow\"></div>");
 					c.writeHtml(pw);
 					pw.println("</td>");
-				}
+				});
 				pw.println("</tr>");
 			}
-			pw.println("</td></tr>");
+			// pw.println("</td></tr>");
 			pw.println("</table>");
 			
 		}
@@ -249,18 +287,24 @@ public class Explainer {
 	}
 	
 	public static class DuplicatesRemovalNode extends Node {
+		public Boolean usingTemporaryTable;
 		public DuplicatesRemovalNode() {
 			super("duplicates_removal", false); 
 		}
 		protected void writeHtml(PrintWriter pw) {
 			pw.println("<table display=\"display: inline-table;\">"); // of course it is
-			pw.println("<tr><td style=\"background-color: yellow;\">DISTINCT</td></tr>"); // tooltip: tableName, usingTemporaryTable: true
-			pw.println("<tr><td>tmp table</td></tr>");
+			pw.println("<tr><td><div class=\"duplicatesRemoval\">DISTINCT</div></td></tr>"); // tooltip: tableName, usingTemporaryTable: true
+			if (usingTemporaryTable) {
+				pw.println("<tr><td><div class=\"tempTableName\">tmp table</div></td></tr>"); // tooltip: tableName, usingTemporaryTable: true
+			}
+			
+			// pw.println("<tr><td>tmp table</td></tr>");
 			if (this.children.size() > 0) {
+				// just has a single NestedLoop child
 				pw.println("<tr>");
 				for (Node c : this.children) {
 					pw.println("<td>");
-					pw.println("<div>^</div>");
+					pw.println("<div class=\"upArrow\"></div>");
 					c.writeHtml(pw);
 					pw.println("</td>");
 				}
@@ -273,27 +317,97 @@ public class Explainer {
 	
 	public static class NestedLoopNode extends Node {
 		public NestedLoopNode() {
-			super("nested_loop", true);
+			super("nestedLoop", true);
 		}
+		
+		// probably need a getWidth() on nodes
+		// so that we can align things above them properly
 		
 		protected void writeHtml(PrintWriter pw) {
 			pw.println("<table display=\"display: inline-table;\">"); // of course it is
+			
+			// OK so my theory is that the 'prefixCost' is a cumulative cost of all the costs leading up to this node ( before = prefix )
+			
+			// on mysql workbench, the cost value leading out of the nestedLoop is the prefixCost of the last node
+			// and the row count leading out of the nestedLoop is the rowsProducedPerJoin of the last node
+			
+			// and the values above each table node is the evalCost + readCost of that table
+			
+			
 			if (this.children.size() > 0) {
+				for (int i = 0; i < this.children.size(); i++) {
+					TableNode c = (TableNode) this.children.get(i);
+					if (i == 0) {
+						pw.println("<td>");
+						pw.println("</td>");
+						pw.println("<td></td>"); // gap for right arrow
+						
+					} else {
+						pw.println("<td><div class=\"queryCost\">");
+						pw.println(c.costInfo==null || c.costInfo.prefixCost == null ? "" : c.costInfo.prefixCost);
+
+						pw.println("</div></td>");
+						if (i < this.children.size() - 1) {
+							pw.println("<td></td>"); // gap for right arrow
+						}
+					}
+				}
+				pw.println("</tr>");
+
+				
+				
 				pw.println("<tr>");
-				for (Node c : this.children) {
-					pw.println("<td>");
-					pw.println("<div>nested loop</div>"); // tooltip: prefix cost
-					pw.println("</td>");
+				for (int i = 0; i < this.children.size(); i++) {
+					TableNode c = (TableNode) this.children.get(i);
+					if (i == 0) {
+						pw.println("<td>");
+						// probably going to have to use SVG here aren't I
+						// holy crap I can't believe that almost worked
+						pw.println("<svg width=\"60\" height=\"60\"></svg>");
+						// pw.println("<div>nested loop</div>"); // tooltip: prefix cost
+						pw.println("</td>");
+						pw.println("<td class=\"rightArrow\"><div class=\"rightArrow\"></div>");
+						pw.println("</td>");
+						
+					} else {
+						pw.println("<td>");
+						// probably going to have to use SVG here aren't I
+						// holy crap I can't believe that almost worked
+						pw.println("<svg width=\"60\" height=\"60\"><g><path fill=\"none\" stroke=\"black\" d=\"M 30,0 L 60 30 L 30 60 L 0 30 L 30 0\"></path>");
+						pw.println("<text x=\"30\" y=\"25\" font-size=\"10px\" dominant-baseline=\"middle\" text-anchor=\"middle\">nested</text>");
+						pw.println("<text x=\"30\" y=\"35\" font-size=\"10px\" dominant-baseline=\"middle\" text-anchor=\"middle\">loop</text>");
+						pw.println("</g></svg>");
+						// pw.println("<div>nested loop</div>"); // tooltip: prefix cost
+						pw.println("</td>");
+						if (i < this.children.size() - 1) {
+							pw.println("<td class=\"rightArrow\">");
+							pw.println("<div class=\"nestedLoopRightArrowLabel\">" + (c.rowsProducedPerJoin == null ? "" :
+								c.rowsProducedPerJoin + (c.rowsProducedPerJoin == 1 ? " row" : " rows")) + "</div>");
+							pw.println("<div class=\"rightArrow\"></div>");
+							pw.println("</td>");
+						}
+					}
 				}
 				pw.println("</tr>");
 			}
 			if (this.children.size() > 0) {
 				pw.println("<tr>");
-				for (Node c : this.children) {
+				for (Node n : this.children) {
+					TableNode c = (TableNode) n;
 					pw.println("<td>");
-					pw.println("<div>^</div>");
+					pw.println("<div><div class=\"upArrow\"></div>");
 					c.writeHtml(pw);
 					pw.println("</td>");
+					if (c.attachedSubqueries == null) {
+						pw.println("<td>"); // gap for right arrow
+						pw.println("</td>");
+					} else {
+						// that bit.
+						pw.println("<td>"); // gap for right arrow
+						// pw.println("THE ATTACHED SUBQUERY");
+						c.attachedSubqueries.writeHtml(pw);
+						pw.println("</td>");
+					}
 				}
 				pw.println("</tr>");
 			}
@@ -303,37 +417,85 @@ public class Explainer {
 	}
 	
 	public static class TableNode extends Node {
+		public CostInfo costInfo;
+		public AccessTypeEnum accessType;
+		public String tableName;
+		public String key;
+		public Long rowsExaminedPerScan;
+		public Long rowsProducedPerJoin;
+		public AttachedSubqueriesNode attachedSubqueries;
+
 		public TableNode() {
 			super("table", false);
 		}
 		
 		protected void writeHtml(PrintWriter pw) {
 			pw.println("<table display=\"display: inline-table;\">"); // of course it is
-			pw.println("<tr><td style=\"background-color: red\">TABLE</td></tr>");
+			pw.println("<tr><td><div class=\"lhsQueryCost\">");
+			pw.println(costInfo==null ? "" : (costInfo.evalCost == null ? (double) 0 : costInfo.evalCost) +
+					(costInfo.readCost == null ? (double) 0 : costInfo.readCost));
+			pw.println("</div><div class=\"rhsQueryCost\">");
+			pw.println(rowsExaminedPerScan == null ? "" : String.valueOf(rowsExaminedPerScan) + 
+					(rowsExaminedPerScan == 1 ? " row" : "rows")); 
+			pw.println("</div>");
+			
+			pw.println("</td></tr>");
+			
+			pw.println("<tr><td><div class=\"table " + 
+				(accessType==AccessTypeEnum.FULL_TABLE_SCAN ? " fullTableScan" :
+				(accessType==AccessTypeEnum.NON_UNIQUE_KEY ? " nonUniqueKey" :
+				(accessType==AccessTypeEnum.UNIQUE_KEY ? " uniqueKey" : ""))) + "\">");
+			pw.println(
+				(accessType==AccessTypeEnum.FULL_TABLE_SCAN ? "Full Table Scan" :
+				(accessType==AccessTypeEnum.NON_UNIQUE_KEY ? " Non-Unique Key Lookup" :
+				(accessType==AccessTypeEnum.UNIQUE_KEY ? "Unique Key Lookup" : ""))));
+			pw.println("</div></td></tr>");
+			
+			pw.println("<tr><td><div class=\"tableName\">");
+			pw.println(tableName);
+			pw.println("</td></tr>");
+
+			pw.println("<tr><td><div class=\"tableKey\">");
+			pw.println(key == null ? "" : key);
+			pw.println("</td></tr>");
+
 			pw.println("</table>");
 			// @TODO attached subqueries
 		}
 	}
 	
 	public static class AttachedSubqueriesNode extends Node {
+		public QuerySpecificationNode querySpecification;
+
 		public AttachedSubqueriesNode() {
 			super("attached_subqueries", true);
 		}
 		
 		protected void writeHtml(PrintWriter pw) {
-			// @TODO this bit
+			pw.println("<table display=\"display: inline-table;\">"); // of course it is
+			pw.println("<tr><td><div class=\"leftArrow\"></div></td>");
+			pw.println("    <td><div class=\"attachedSubqueries\">attached_subqueries</div></td>");
+			pw.println("</tr>");
+			pw.println("<tr><td></td>"); // gap for leftArrow
+			pw.println("    <td><div class=\"upArrow\"></div></td>");
+			pw.println("</tr>");
+			pw.println("<tr><td></td>");
+			pw.println("    <td>");
+			querySpecification.writeHtml(pw);
+			// the attached subquery
+			pw.println("    </td>");
+			pw.println("</tr>");
+			
+			pw.println("</table>");
+			
 		}
 	}
 	
+
 	
 	
 	
 	public static class ExplainDiagram {
-		/* #
-# JSON Explain Data Parser
-#
-# Takes a JSON object (bunch of dicts and lists) and turns into internal node objects
-# */
 
 		private Node topNode;
 		String json;
@@ -359,7 +521,10 @@ public class Explainer {
 
 		// query_block
 		private Node parseQueryBlock(JSONObject obj) {
-			Node n = new QueryBlockNode();
+			QueryBlockNode n = new QueryBlockNode();
+			n.selectId = toLong(obj.get("select_id"));
+			n.message = obj.get("message");
+			
 			n.attributes.put("selectId", obj.get("select_id"));
 			n.attributes.put("message", obj.get("message"));
 			if (obj.containsKey("cost_info")) { n.attributes.put("costInfo", parseCostInfo((JSONObject) obj.get("cost_info"))); }
@@ -378,6 +543,10 @@ public class Explainer {
 			return n;
 		}
 		
+		private Object toLong(Object object) {
+			return object == null ? null : ((Number) object).longValue();
+		}
+
 		private CostInfo parseCostInfo(JSONObject obj) {
 			CostInfo ci = new CostInfo();
 			if (obj.containsKey("query_cost")) { ci.setQueryCost(Double.parseDouble((String) obj.get("query_cost"))); }
@@ -390,7 +559,9 @@ public class Explainer {
 
 		
 		private Node parseUnionResult(JSONObject obj) {
-			Node n = new UnionResultNode();
+			UnionResultNode n = new UnionResultNode();
+			n.tableName = (String) obj.get("table_name");
+			
 			n.attributes.put("usingTemporaryTable", obj.get("using_temporary_table"));
 			n.attributes.put("tableName", obj.get("table_name"));
 			n.attributes.put("accessType", obj.get("access_type"));
@@ -409,8 +580,8 @@ public class Explainer {
 			return n;
 		}
 		
-		private Node parseQuerySpecification(JSONObject obj) {
-			Node n = new QuerySpecificationNode();
+		private QuerySpecificationNode parseQuerySpecification(JSONObject obj) {
+			QuerySpecificationNode n = new QuerySpecificationNode();
 			n.attributes.put("dependent", obj.get("dependent"));
 			n.attributes.put("cacheable", obj.get("cacheable"));
 			if (obj.containsKey("query_block")) {
@@ -423,7 +594,9 @@ public class Explainer {
 		}
 		
 		private Node parseDuplicatesRemoval(JSONObject obj) {
-			Node n = new DuplicatesRemovalNode();
+			DuplicatesRemovalNode n = new DuplicatesRemovalNode();
+			n.usingTemporaryTable = (Boolean) obj.get("using_temporary_table");
+					
 			n.attributes.put("usingTemporaryTable", obj.get("using_temporary_table"));
 			n.attributes.put("usingFilesort", obj.get("using_filesort"));
 			// n.attributes.put("selectId", obj.get("select_id"));
@@ -436,7 +609,7 @@ public class Explainer {
 				for (Object o : loopItems) {
 					JSONObject cobj = (JSONObject) o;
 					if (cobj.containsKey("table")) {
-						Node cn = parseTable((JSONObject) cobj.get("table"));
+						TableNode cn = parseTable((JSONObject) cobj.get("table"));
 						sn.addChild(cn);
 					} else {
 						throw new IllegalArgumentException("expected table in nested_loop child");
@@ -455,36 +628,40 @@ public class Explainer {
 			return n;
 		}
 		
-		private Node parseTable(JSONObject obj) {
-			Node n = new TableNode();
+		private TableNode parseTable(JSONObject obj) {
+			TableNode n = new TableNode();
+			n.tableName = (String) obj.get("table_name");
 			n.attributes.put("tableName", obj.get("table_name"));
 			n.attributes.put("distinct", obj.get("distinct")); // boolean
+			
+			n.accessType = AccessTypeEnum.fromJsonValue((String) obj.get("access_type"));
 			n.attributes.put("accessType", obj.get("access_type")); // "ALL", "ref", "eq_ref"
 			n.attributes.put("possibleKeys", parseNameList((JSONArray) obj.get("possible_keys"))); // List<String>
-			if (obj.containsKey("key")) { n.attributes.put("key",  obj.get("key")); } // "PRIMARY"
+			if (obj.containsKey("key")) { n.key = (String) obj.get("key"); n.attributes.put("key",  obj.get("key")); } // "PRIMARY"
 			n.attributes.put("usedKeyParts", parseNameList((JSONArray) obj.get("used_key_parts"))); // List<String>, subset of above ?
 			n.attributes.put("usedColumns", parseNameList((JSONArray) obj.get("used_columns"))); // List<String>
 			if (obj.containsKey("key_length")) { n.attributes.put("keyLength", Long.parseLong((String) obj.get("key_length"))); }
 			if (obj.containsKey("ref")) { n.attributes.put("ref", parseNameList((JSONArray) obj.get("ref"))); } // List<String>
 			
-			if (obj.containsKey("rows_examined_per_scan")) { n.attributes.put("rows_examined_per_scan", ((Number) obj.get("rows_examined_per_scan")).longValue()); }
-			if (obj.containsKey("rows_produced_per_join")) { n.attributes.put("rows_produced_per_join", ((Number) obj.get("rows_produced_per_join")).longValue()); }
+			if (obj.containsKey("rows_examined_per_scan")) { n.rowsExaminedPerScan = ((Number) obj.get("rows_examined_per_scan")).longValue(); n.attributes.put("rows_examined_per_scan", n.rowsExaminedPerScan); }
+			if (obj.containsKey("rows_produced_per_join")) { n.rowsProducedPerJoin = ((Number) obj.get("rows_produced_per_join")).longValue(); n.attributes.put("rows_produced_per_join", n.rowsProducedPerJoin); }
 			if (obj.containsKey("filtered")) { n.attributes.put("filtered", Double.parseDouble((String) obj.get("filtered"))); }
 			n.attributes.put("index_condition", obj.get("index_condition"));
 			
 			
-			if (obj.containsKey("cost_info")) { n.attributes.put("costInfo", parseCostInfo((JSONObject) obj.get("cost_info"))); }
+			if (obj.containsKey("cost_info")) { n.costInfo = parseCostInfo((JSONObject) obj.get("cost_info")); n.attributes.put("costInfo", n.costInfo); }
 			n.attributes.put("attachedCondition", obj.get("attached_condition"));
 			if (obj.containsKey("attached_subqueries")) {
-				Node sn = new AttachedSubqueriesNode();
+				AttachedSubqueriesNode sn = new AttachedSubqueriesNode();
+				n.attachedSubqueries = sn;
 				n.addChild(sn);
 				JSONArray qs = (JSONArray) obj.get("attached_subqueries");
 				for (Object o : qs) {
 					JSONObject cobj = (JSONObject) o;
-					Node cn = parseQuerySpecification((JSONObject) o);
-					sn.addChild(cn);
+					QuerySpecificationNode qsn = parseQuerySpecification((JSONObject) o);
+					sn.querySpecification = qsn;
+					sn.addChild(qsn);
 				}
-				n.addChild(sn);
 			}
 			return n;
 		}
@@ -496,7 +673,22 @@ public class Explainer {
 
 		public void writeHtml(PrintWriter pw) {
 			pw.println("<html>");
-			pw.println("<head><title>Here we go</title></head>");
+			pw.println("<head><title>Here we go</title>");
+			pw.println("<style>");
+			
+			InputStream is = Explainer.class.getResourceAsStream("/css.css");
+			String s;
+			try {
+				s = new String(StreamUtil.getByteArray(is));
+			} catch (IOException e) {
+				throw new IllegalStateException("IOException", e);
+			}
+			pw.println(s);
+			
+			// pw.println("TD { vertical-align: top; }");
+			pw.println("</style>");
+			pw.println("</head>");
+			
 			pw.println("<body>");
 			topNode.writeHtml(pw);
 			
@@ -538,7 +730,7 @@ public class Explainer {
 		
 		ExplainDiagram ec = new ExplainDiagram(json, "1.2.3");
 		
-		// roundtrip attempt
+		// roundtrip attemptt
 		json = ec.toJson();
 		System.out.println(json);
 		obj = (JSONObject) jp.parse(json);
