1f73055134df9ec262af72e537bce142164c708c
Greg Knox <knoxg@randomnoun.com>
2022-08-22 06:47:17 +1000

    generate test outputs for unit tests

--- src/test/java/com/randomnoun/common/db/explain/SqlExplainToImageTest.java
+++ src/test/java/com/randomnoun/common/db/explain/SqlExplainToImageTest.java
@@ -2,10 +2,12 @@ package com.randomnoun.common.db.explain;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -27,6 +29,8 @@ public class SqlExplainToImageTest extends TestCase {
 
 	Logger logger = Logger.getLogger(SqlExplainToImageTest.class);
 	
+    private static boolean WRITE_EXPECTED_OUTPUT = false;	
+	
 	public void setUp() throws IOException {
 		Log4jCliConfiguration lcc = new Log4jCliConfiguration();
 		lcc.init("[explain-to-image]", null);
@@ -40,51 +44,56 @@ public class SqlExplainToImageTest extends TestCase {
 	
 	public void testSomeThings() throws IOException, ParseException {
 		
-		testParser("sakila/sakila-7g.json");
-		testSvg("sakila/sakila-7g.json");
+		testParser("sakila/sakila-7g"); // .json
+		testSvg("sakila/sakila-7g");
 	}
 	
 	public void testParser(String resourceName) throws IOException, ParseException {
-		InputStream is = SqlExplainToImage.class.getResourceAsStream("/" + resourceName);
+		InputStream is = SqlExplainToImage.class.getResourceAsStream("/" + resourceName + ".json");
 		ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
 		StreamUtil.copyStream(is, baos);
 		String json = baos.toString();
-		
 		CompactJsonWriter w = new CompactJsonWriter();
 		JSONParser jp = new JSONParser();
 		JSONObject obj = (JSONObject) jp.parse(json);
+		
+		/*
 		// ByteArrayOutputStream out1 = new ByteArrayOutputStream();
 		FileOutputStream out1 = new FileOutputStream("target/test/" + resourceName + "-compact.json");
 		PrintWriter pw = new PrintWriter(out1);
 		w.writeJSONValue(obj, pw);
 		pw.flush();
 		out1.close();
-		
+		*/
 		
 		PlanParser pp = new PlanParser(json, "1.2.3");
-		
-		// roundtrip attempt
 		json = pp.toJson();
-		// System.out.println(json);
 		obj = (JSONObject) jp.parse(json);
-		FileOutputStream out2 = new FileOutputStream("target/test/" + resourceName + "-roundtrip.json");
-		pw = new PrintWriter(out2);
-		w.writeJSONValue(obj, pw);
-		pw.flush();
-		// System.out.println(out1.toString());
-		out2.close();
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        w.writeJSONValue(obj, pw);
+        pw.flush();
+        
+        File tf = new File("src/test/resources/expected-output/" + resourceName + "-roundtrip.json");
+        if (WRITE_EXPECTED_OUTPUT) {
+            FileOutputStream fos = new FileOutputStream(tf);
+            fos.write(sw.toString().getBytes());
+    		fos.close();
+        } else {
+            FileInputStream fis = new FileInputStream(tf);
+            String expected = new String(StreamUtil.getByteArray(fis));
+            fis.close();
+            assertEquals("difference in " + resourceName + "-roundtrip.json", expected.trim(), sw.toString().trim());
+        }
 	}
 	
 	public void testSvg(String resourceName) throws IOException, ParseException {
-		InputStream is = SqlExplainToImage.class.getResourceAsStream("/" + resourceName);
+		InputStream is = SqlExplainToImage.class.getResourceAsStream("/" + resourceName + ".json");
 		ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
 		StreamUtil.copyStream(is, baos);
 		String json = baos.toString();
 		
 		PlanParser pp = new PlanParser(json, "1.2.3");
-		
-		// roundtrip attempt
-		json = pp.toJson();
 		
 		// diagram attempts
 		Layout layout = new Layout(pp.getTopNode());
@@ -101,17 +110,24 @@ public class SqlExplainToImageTest extends TestCase {
 		ReweightBoxVisitor rwv = new ReweightBoxVisitor(rv.getMinWeight(), rv.getMaxWeight());
 		b.traverse(rwv);
 		
-		FileOutputStream out3 = new FileOutputStream("target/test/" + resourceName + ".html");
-		PrintWriter pw = new PrintWriter(out3);
-		SvgBoxVisitor sbv = new SvgBoxVisitor(pw);
+		StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        SvgBoxVisitor sbv = new SvgBoxVisitor(pw);
 		b.traverse(sbv);
 		pw.flush();
-		out3.close();
 		
-		PrintWriter sysPw = new PrintWriter(System.out);
-		sbv = new SvgBoxVisitor(sysPw);
-		b.traverse(sbv);
-		sysPw.flush();
+		File tf = new File("src/test/resources/expected-output/" + resourceName + ".html");
+        if (WRITE_EXPECTED_OUTPUT) {
+            FileOutputStream fos = new FileOutputStream(tf);
+            fos.write(sw.toString().getBytes());
+    		fos.close();
+        } else {
+            FileInputStream fis = new FileInputStream(tf);
+            String expected = new String(StreamUtil.getByteArray(fis));
+            fis.close();
+            assertEquals("difference in " + resourceName + ".html", expected.trim(), sw.toString().trim());
+        }
+        
 	}
 	
 }
