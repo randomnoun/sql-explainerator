db9f579a7fbfcf8984e24537f796168aa397a9a2
knoxg <knoxg@EXCIMER.mshome.net>
2022-09-24 18:11:01 +1000

    typo

--- src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorSaqilaTest.java
+++ src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorSaqilaTest.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.json.simple.parser.ParseException;
 
-public class SqlExplaineratprSaqilaTest extends SqlExplaineratorTestBase {
+public class SqlExplaineratorSaqilaTest extends SqlExplaineratorTestBase {
 
-	Logger logger = Logger.getLogger(SqlExplaineratprSaqilaTest.class);
+	Logger logger = Logger.getLogger(SqlExplaineratorSaqilaTest.class);
 	
 	public void testSomeThings() throws IOException, ParseException {
 		testParser("sakila/sakila-7g");
