836395a5e8c004daff08fb49f7f0eb77589d48c3
knoxg <knoxg@EXCIMER.mshome.net>
2022-09-25 07:58:14 +1000

    more sakila test inputs

--- src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorSaqilaTest.java
+++ src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorSaqilaTest.java
@@ -10,6 +10,21 @@ public class SqlExplaineratorSaqilaTest extends SqlExplaineratorTestBase {
 	Logger logger = Logger.getLogger(SqlExplaineratorSaqilaTest.class);
 	
 	public void testSomeThings() throws IOException, ParseException {
+		testParser("sakila/sakila-1");
+		testSvg("sakila/sakila-1");
+
+		testParser("sakila/sakila-4a");
+		testSvg("sakila/sakila-4a");
+
+		testParser("sakila/sakila-6a");
+		testSvg("sakila/sakila-6a");
+
+		testParser("sakila/sakila-6b");
+		testSvg("sakila/sakila-6b");
+		
+		testParser("sakila/sakila-6c");
+		testSvg("sakila/sakila-6c");
+		
 		testParser("sakila/sakila-7g");
 		testSvg("sakila/sakila-7g");
 	}
