8c49cf739fe59de794ee318aa8841dc6985cd05f
Greg Knox <knoxg@randomnoun.com>
2025-05-19 14:50:18 +1000

    expected output

--- src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorTestBase.java
+++ src/test/java/com/randomnoun/common/db/explain/SqlExplaineratorTestBase.java
@@ -31,7 +31,7 @@ public abstract class SqlExplaineratorTestBase extends TestCase {
 
 	Logger logger = Logger.getLogger(SqlExplaineratorTestBase.class);
 
-	private static boolean WRITE_EXPECTED_OUTPUT = true;
+	private static boolean WRITE_EXPECTED_OUTPUT = false;
 
 	public void setUp() throws IOException {
 		Log4jCliConfiguration lcc = new Log4jCliConfiguration();
